d0095d9a13235c79a74d9955f68d04810a514c02
Andreja Milosavljevic <77466950+doomguy-369@users.noreply.github.com>
2021-12-20 09:13:04 +0100

    Add files via upload

---
+++ hack_nasa.sh
@@ -0,0 +1,39 @@
+figlet Starting Attack on NASA
+echo
+read -t 0.5
+echo "NASA Hacked 0%"
+echo
+read -t 0.5
+echo "NASA Hacked 10%"
+echo
+read -t 0.5
+echo "NASA Hacked 20%"
+echo
+read -t 0.5
+echo "NASA Hacked 30%"
+echo
+read -t 0.5
+echo "NASA Hacked 40%"
+echo
+read -t 0.5
+echo "NASA Hacked 50%"
+echo
+read -t 0.5
+echo "NASA Hacked 60%"
+echo
+read -t 0.5
+echo "NASA Hacked 70%"
+echo
+read -t 0.5
+echo "NASA Hacked 80%"
+echo
+read -t 0.5
+echo "NASA Hacked 90%"
+echo
+read -t 0.5
+echo "NASA Hacked 100%"
+echo
+read -t 0.5
+echo
+echo "NASA Hacked succsessfuly..."
+echo "... you're all in Boss !!!"
